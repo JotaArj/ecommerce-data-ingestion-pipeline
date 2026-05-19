7587e26840f52f907510a3508cc64b3c37a8bfc0
Jacinto Arjona Garrido <arjona.g@gmail.com>
2026-05-19 08:22:59 +0200

    activate run_catalog

--- src/ecommerce_ingestion/app/main.py
+++ src/ecommerce_ingestion/app/main.py
@@ -1,11 +1,11 @@
-#from ecommerce_ingestion.app.run_catalog import run_catalog
-#from ecommerce_ingestion.config.constants import DEFAULT_SOURCE_SITE
+from ecommerce_ingestion.app.run_catalog import run_catalog
 from ecommerce_ingestion.app.run_silver import run_silver
+from ecommerce_ingestion.config.constants import DEFAULT_SOURCE_SITE
 
 
 def main() -> None:
 
-    #run_catalog(DEFAULT_SOURCE_SITE)
+    run_catalog(DEFAULT_SOURCE_SITE)
     run_silver()
 
 
